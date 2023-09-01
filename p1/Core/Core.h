72bf5945eb2a78d1c882a5dbfe1ea6512abd8588
KnOFCA <knofca@foxmail.com>
2023-09-01 21:57:05 +0800

    new arch

---
+++ p1/Core/Core.h
@@ -0,0 +1,13 @@
+
+namespace Core
+{
+	struct Point
+	{
+		int x, y;
+		Point() :x(), y() {};
+		Point(int& x, int& y) : x(x), y(y) {};
+		Point(const Point& other) : x(other.x), y(other.y) {};
+	};
+
+
+}
--- p1/Starter/D2D.cpp
+++ p1/Starter/D2D.cpp
@@ -1,10 +1,12 @@
-#if 1
+#include "APISwitch.h"
+#ifdef D2D
+
 #include <windows.h>
 #include <d2d1.h>
 #include <dwmapi.h>
+#include <windowsx.h>
 #pragma comment(lib, "d2d1")
 #pragma comment(lib, "Dwmapi")
-
 
 #include "BaseWindow.hpp"
 
@@ -18,13 +20,35 @@ void SafeRelease(T** ppT)
 	}
 }
 
+//DPI helper
+class DPIScale
+{
+	static float scale;
+
+public:
+	static void Initialize(HWND hwnd)
+	{
+		float dpi = GetDpiForWindow(hwnd);
+		scale = dpi / 96.0f;
+	}
+
+	template <typename T>
+	static D2D1_POINT_2F PixelsToDips(T x, T y)
+	{
+		return D2D1::Point2F(static_cast<float>(x) / scale, static_cast<float>(y) / scale);
+	}
+};
+
+float DPIScale::scale = 1.0f;
+
 class MainWindow : public BaseWindow<MainWindow>
 {
 	ID2D1Factory* pFactory;
 	ID2D1HwndRenderTarget* pRenderTarget;
 	//user define
 	ID2D1SolidColorBrush* pBrush;
-	D2D1_ELLIPSE            ellipse;
+	D2D1_ELLIPSE ellipse;
+	D2D1_POINT_2F ptMouse;
 	//user define end
 
 	void    CalculateLayout();
@@ -32,6 +56,9 @@ class MainWindow : public BaseWindow<MainWindow>
 	void    DiscardGraphicsResources();
 	void    OnPaint();
 	void    Resize();
+	void    OnLButtonDown(int pixelX, int pixelY, DWORD flags);
+	void    OnLButtonUp();
+	void    OnMouseMove(int pixelX, int pixelY, DWORD flags);
 
 public:
 
@@ -56,7 +83,8 @@ void MainWindow::CalculateLayout()
 		const float x = size.width / 2;
 		const float y = size.height / 2;
 		const float radius = min(x, y);
-		ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+		//todo
+		ellipse = D2D1::Ellipse(D2D1::Point2F(x+ ptMouse.x/* - pixelX*/, y), radius, radius);
 		//user define end
 	}
 }
@@ -79,7 +107,7 @@ HRESULT MainWindow::CreateGraphicsResources()
 		if (SUCCEEDED(hr))
 		{
 			//user define
-			const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0.0f);
+			const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0.0f, 1.0f);
 			hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
 			//user define end
 
@@ -109,7 +137,7 @@ void MainWindow::OnPaint()
 		pRenderTarget->BeginDraw();
 
 		//user define
-		pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+		pRenderTarget->Clear(D2D1::ColorF(0.0f, 1.0f, 1.0f, 0.5f));
 		pRenderTarget->FillEllipse(ellipse, pBrush);
 		//user define end
 
@@ -137,6 +165,33 @@ void MainWindow::Resize()
 	}
 }
 
+void MainWindow::OnLButtonDown(int pixelX, int pixelY, DWORD flags)
+{
+	SetCapture(m_hwnd);
+	ptMouse.x = pixelX;
+	ptMouse.y = pixelY;
+	InvalidateRect(m_hwnd, NULL, FALSE);
+}
+
+void MainWindow::OnMouseMove(int pixelX, int pixelY, DWORD flags)
+{
+	if (flags & MK_LBUTTON)
+	{
+		RECT rc;
+		GetWindowRect(m_hwnd, &rc);
+
+		pRenderTarget->SetTransform(D2D1::Matrix3x2F::Translation(D2D1_SIZE_F( ptMouse.x - pixelX , ptMouse.y - pixelY )));
+		MoveWindow(m_hwnd, rc.left + pixelX - ptMouse.x, rc.top + pixelY - ptMouse.y, rc.right - rc.left, rc.bottom - rc.top, true);
+
+		InvalidateRect(m_hwnd, NULL, FALSE);
+	}
+}
+
+void MainWindow::OnLButtonUp()
+{
+	ReleaseCapture();
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
 	MainWindow win;
@@ -148,13 +203,13 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 
 	ShowWindow(win.Window(), nCmdShow);
 
-	//set transparent background
-	UpdateWindow(win.Window());
+	////set transparent background
+	//UpdateWindow(win.Window());
 
-	LONG ret = GetWindowLong(win.Window(), GWL_EXSTYLE);
-	ret = ret | WS_EX_LAYERED;
-	SetWindowLong(win.Window(), GWL_STYLE, GetWindowLong(win.Window(), GWL_STYLE) & ~WS_CAPTION);
-	//end
+	//LONG ret = GetWindowLong(win.Window(), GWL_EXSTYLE);
+	//ret = ret | WS_EX_LAYERED;
+	//SetWindowLong(win.Window(), GWL_STYLE, GetWindowLong(win.Window(), GWL_STYLE) & ~WS_CAPTION);
+	////end
 
 	// Run the message loop.
 
@@ -167,6 +222,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 
 	return 0;
 }
+
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -187,6 +243,7 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 			rcClient.left, rcClient.top,
 			rcClient.right, rcClient.bottom,
 			SWP_FRAMECHANGED);
+		DPIScale::Initialize(m_hwnd);
 		return 0;
 
 	case WM_DESTROY:
@@ -207,7 +264,25 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 		wParam = 1;
 		return 0;
 
-		//case
+	case WM_LBUTTONDOWN:
+		OnLButtonDown(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), (DWORD)wParam);
+		return 0;
+
+	case WM_LBUTTONUP:
+		OnLButtonUp();
+		return 0;
+
+	case WM_MOUSEMOVE:
+		OnMouseMove(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam), (DWORD)wParam);
+		return 0;
+
+	case WM_SETCURSOR:
+		if (LOWORD(lParam) == HTCLIENT)
+		{
+			SetCursor(LoadCursor(NULL,IDC_HAND));
+			return TRUE;
+		}
+		break;
 	}
 	// Handle the window activation.
 	if (uMsg == WM_ACTIVATE)
@@ -230,4 +305,5 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
 }
-#endif
+
+#endif // D2D
